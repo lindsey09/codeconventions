95b2cec52b22513f42645e06e568b3ae4fa974ea
Lindsey Kim <mmommm99@gmail.com>
2022-08-26 15:20:29 +0900

    test upload

--- menu_Slidedown/menu_down.js
+++ menu_Slidedown/menu_down.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-    $('.header .header-nav > li').hover(function(){
-        $('.header .header-nav > li > ul').slideToggle();
+    $('.header .header-nav > li').hover(function(e){
+        $('.header .header-nav > li > ul').slideToggle(1000);
+        e.stopPropagaton();
     })
 
 });
